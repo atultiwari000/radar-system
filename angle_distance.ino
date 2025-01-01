e084b8cd446f1bd5539c4dbbc0755e9c6eb0df2d
atultiwari000 <tiwari.000atul@gmail.com>
2025-01-01 23:07:07 +0545

    Radar system inital commit

---
+++ angle_distance.ino
@@ -0,0 +1,48 @@
+
+#include <Servo.h>. 
+const int trigPin = 9;
+const int echoPin = 10;
+
+long duration;
+int distance;
+Servo myServo; 
+void setup() {
+  pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin, INPUT); 
+  Serial.begin(9600);
+  myServo.attach(8); 
+}
+void loop() {
+  for(int i=0;i<=180;i++){  
+  myServo.write(i);
+  delay(30);
+  distance = calculateDistance();
+  
+  Serial.print(i); 
+  Serial.print(","); 
+  Serial.print(distance); 
+  Serial.print("."); 
+  }
+
+  for(int i=180;i>0;i--){  
+  myServo.write(i);
+  delay(30);
+  distance = calculateDistance();
+  Serial.print(i);
+  Serial.print(",");
+  Serial.print(distance);
+  Serial.print(".");
+  }
+}
+int calculateDistance(){ 
+  
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin, HIGH); 
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH); 
+  distance= duration*0.034/2;
+  return distance;
+}
